516c960c19444b54df1de945c4047f079e4c02a5
Manjot Kaur <manjotkaur7@academic.rrc.ca>
2023-11-20 19:30:49 -0600

    Working on issue #3: Added documentation for OutputHandler class

--- output_handler/output_handler.py
+++ output_handler/output_handler.py
@@ -2,28 +2,45 @@ import csv
 
 class OutputHandler:
     """
-    REQUIRED: CLASS DOCSTRING
+    This class handles and exports transactional data to CSV file.
     """
 
     def __init__(self, account_summaries: dict, 
                        suspicious_transactions: list, 
                        transaction_statistics: dict) -> None:
+        
         """
-        REQUIRED: METHOD DOCSTRING
+            Initialize the OutputHandler instance using the account summaries, 
+        suspicious transactions and transaction statistics.
+
+        Args:
+            account_summaries (dict) : A dictionary to store the summary of accounts.
+            suspicious_transactions (list) : A list to store suspicious transactions.
+            transaction_statistics (dict) : A dictionary to store transaction statistics.
+
+        Returns:
+            None
         """
         self._account_summaries = account_summaries
         self._suspicious_transactions = suspicious_transactions
         self._transaction_statistics = transaction_statistics
     
-
     def write_account_summaries_to_csv(self, file_path: str) -> None:
         """
-        REQUIRED: METHOD DOCSTRING
+        This method writes the account summaries to the CSV file.
+
+        Args:
+            file_path (str) : The path of the CSV file.
+
+        Returns:
+            None 
         """
+
         with open(file_path, 'w', newline='') as output_file:
             writer = csv.writer(output_file)
             writer.writerow(['Account number', 'Balance', 'Total Deposits', 'Total Withdrawals'])
 
+            # Write account summaries to the CSV file.
             for account_number, summary in self._account_summaries.items():
                 writer.writerow([
                     account_number,
@@ -34,12 +51,20 @@ class OutputHandler:
 
     def write_suspicious_transactions_to_csv(self, file_path: str) -> None:
         """
-        REQUIRED: METHOD DOCSTRING
+        This method writes the suspicious transactions to the CSV file.
+
+        Args:
+            file_path (str) : The path of the CSV file.
+
+        Returns:
+            None
         """
+
         with open(file_path, 'w', newline='') as output_file:
             writer = csv.writer(output_file)
             writer.writerow(['Transaction ID', 'Account number', 'Date', 'Transaction type', 'Amount', 'Currency', 'Description'])
 
+            # Write suspicious transactions to the CSV file.
             for transaction in self._suspicious_transactions:
                 writer.writerow([
                     transaction['Transaction ID'],
@@ -53,12 +78,20 @@ class OutputHandler:
 
     def write_transaction_statistics_to_csv(self, file_path: str) -> None:
         """
-        REQUIRED: METHOD DOCSTRING
-        """        
+        This method writes the transaction statistics to the CSV file.
+
+        Args:
+            file_path (str) : The path of the CSV file.
+
+        Returns:
+            None
+        """    
+
         with open(file_path, 'w', newline='') as output_file:
             writer = csv.writer(output_file)
             writer.writerow(['Transaction type', 'Total amount', 'Transaction count'])
 
+            # Write transaction statistics to the CSV file.
             for transaction_type, statistic in self._transaction_statistics.items():
                 writer.writerow([
                     transaction_type,
